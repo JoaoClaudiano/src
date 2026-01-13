6895fc77df1abcbafec14fe34e1178d01152f2bb
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-13 20:23:49 -0300

    Implement RoomGenerator class for room generation

---
+++ procedural/RoomGenerator.js
@@ -0,0 +1,10 @@
+import { ROOM_TEMPLATES } from "./RoomTemplates";
+import { RNG } from "./RNG";
+export class RoomGenerator {
+    constructor(seed) {
+        this.rng = new RNG(seed);
+    }
+    generate() {
+        return this.rng.pick(ROOM_TEMPLATES);
+    }
+}
