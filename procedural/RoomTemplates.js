5eb9c70477e4fcd4da5f0b045a187402dbb06aa3
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-13 20:27:18 -0300

    Add ROOM_TEMPLATES for different enemy types

---
+++ procedural/RoomTemplates.js
@@ -0,0 +1,20 @@
+export const ROOM_TEMPLATES = [
+    {
+        id: "normal_easy",
+        type: "normal",
+        enemyCount: 3,
+        enemyHPModifier: 1
+    },
+    {
+        id: "normal_hard",
+        type: "normal",
+        enemyCount: 6,
+        enemyHPModifier: 1.5
+    },
+    {
+        id: "elite",
+        type: "elite",
+        enemyCount: 8,
+        enemyHPModifier: 2
+    }
+];
