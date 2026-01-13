7fc2e7d3fb010330ad88b7c3f151a6d784ecf147
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-13 18:48:32 -0300

    Add ROOM_TEMPLATES for room definitions

---
+++ procedural/RoomTemplates.ts
@@ -0,0 +1,22 @@
+import { RoomTemplateDef } from "./RoomTemplateDef"
+
+export const ROOM_TEMPLATES: RoomTemplateDef[] = [
+  {
+    id: "normal_easy",
+    type: "normal",
+    enemyCount: 3,
+    enemyHPModifier: 1
+  },
+  {
+    id: "normal_hard",
+    type: "normal",
+    enemyCount: 6,
+    enemyHPModifier: 1.5
+  },
+  {
+    id: "elite",
+    type: "elite",
+    enemyCount: 8,
+    enemyHPModifier: 2
+  }
+]
