32d581bd48518280662e0689aa12fc2297bf8ca8
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-13 19:36:42 -0300

    Simplify vite.config.ts configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/src/', // nome do seu repo no GitHub Pages
-  build: {
-    outDir: 'dist'
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+  base: '/src/',
+  build: { outDir: 'dist' },
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } }
 })
