b3e4e666879af3741104688c5f454891f3f13843
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-13 20:01:51 -0300

    Initialize Phaser game with RoomScene

---
+++ main.js
@@ -0,0 +1,13 @@
+import Phaser from "phaser";
+import { RoomScene } from "./scenes/RoomScene";
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: "game",
+    backgroundColor: "#222222",
+    physics: { default: "arcade", arcade: { debug: false } },
+    scene: [RoomScene]
+};
+//novas fases
+new Phaser.Game(config);
